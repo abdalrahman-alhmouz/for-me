6b6addddfce6914a617ec705f6625c0681b0fe21
abdalrahman-alhmouz <abood.alhmouz121@gmail.com>
2020-12-19 20:53:34 +0200

    init comit

---
+++ me.js
@@ -0,0 +1,32 @@
+prompt ("Are you animal lover?");
+var number = 11 ;
+var pepsi = "cola" ;
+var correct = true  ;
+var notcorrect = false ;
+
+function cheklargnumber () {
+    if (number <= 10) {
+        alert ("welcom to my life")
+    } else {alert("we hope you are engoy")}
+    
+}
+cheklargnumber ("5");
+function chekcola () {
+    if (pepsi == "cola") {
+        alert ("yes") ;      
+    } else {alert ("no")}
+    
+}
+
+chekcola () ;
+function thename () {
+    var name = prompt ("what is your name ?");
+    var password = prompt ("what is your passowrd?") ;
+alert ("HELLO " + name)   
+}
+thename () ;
+ let myname = prompt("inser your name") ;
+ while (myname !== "abood") {
+     myname == promot ("insert abood");
+     
+ }
